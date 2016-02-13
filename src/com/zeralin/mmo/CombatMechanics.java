cc8eb6492035eea2243d80b55e5fca4fb20a6817
Zeralin <zeralinservers@yahoo.com>
2016-02-13 11:35:45 +0000

    gg

--- src/com/zeralin/mmo/CombatMechanics.java
+++ src/com/zeralin/mmo/CombatMechanics.java
@@ -1,10 +1,12 @@
 package com.zeralin.mmo;
 
 import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import net.md_5.bungee.api.ChatColor;
@@ -23,8 +25,89 @@ public class CombatMechanics implements Listener{
 	}
 	
 	@EventHandler
+	public void onWorldDamage(EntityDamageEvent e){
+		if (e.getEntity() instanceof Player) {
+			Player player = (Player) e.getEntity();
+			if (e.getDamage() <= 0) {
+				return;
+			}
+			if (player.getNoDamageTicks() <= player.getMaximumNoDamageTicks() / 2 && player.getHealth() > 0.0D) {
+				int dmg = (int) e.getDamage();
+				if (dmg <= 0) {
+					return;
+				}
+				int health = 0;
+				if (player.getHealth() - (double) dmg > 0.0D) {
+					health = (int) player.getHealth() - dmg;
+				}
+				if (health >= 1){
+					player.sendMessage(ChatColor.RED + "" + dmg + " -> " + ChatColor.WHITE + "[" + 
+				                ChatColor.GREEN + health + ChatColor.WHITE + "]");
+				}
+			}
+			}
+	}
+	
+	@EventHandler
+	public void onDamageTake(EntityDamageByEntityEvent e){
+		if (e.getEntity() instanceof Player && e.getDamager() instanceof Entity){
+			Player player = (Player) e.getEntity();
+			Creature mob = (Creature) e.getDamager();
+			
+			if (e.getDamage() == 0){
+				return;
+			} else {
+				
+				int dmg = (int) e.getDamage();
+				int hp = (int) player.getHealth() - dmg;
+				
+				if (hp > 0){
+					player.setHealth(hp);
+				} else {
+					player.setHealth(0);
+				}
+				
+				e.setDamage(0);
+				if (hp >= 1){
+					player.sendMessage(ChatColor.RED + "" + dmg + " -> " + ChatColor.WHITE + "[" + 
+				                ChatColor.GREEN + hp + ChatColor.WHITE + "]");
+					} else {
+						player.sendMessage(ChatColor.RED + "" + dmg + " -> " + mob.getCustomName() + 
+								ChatColor.WHITE + " [" + ChatColor.GREEN + "0" + ChatColor.WHITE + "]");
+					}
+			}
+		} else if (e.getEntity() instanceof Player && e.getDamager() instanceof Player){
+			Player player = (Player) e.getEntity();
+			Player mob = (Player) e.getDamager();
+			
+			if (e.getDamage() == 0){
+				return;
+			} else {
+				
+				int dmg = (int) e.getDamage();
+				int hp = (int) player.getHealth() - dmg;
+				
+				if (hp > 0){
+					player.setHealth(hp);
+				} else {
+					player.setHealth(0);
+				}
+				
+				e.setDamage(0);
+				if (hp >= 1){
+					player.sendMessage(ChatColor.RED + "" + dmg + " -> " + ChatColor.WHITE + "[" + 
+				                ChatColor.GREEN + hp + ChatColor.WHITE + "]");
+					} else {
+						player.sendMessage(ChatColor.RED + "" + dmg + " -> " + mob.getCustomName() + 
+								ChatColor.WHITE + " [" + ChatColor.GREEN + "0" + ChatColor.WHITE + "]");
+					}
+			}
+		}
+	}
+	
+	@EventHandler
 	public void onOutgoingDamage(EntityDamageByEntityEvent e){
-		if (e.getDamager() instanceof Player){
+		if (e.getDamager() instanceof Player && !(e.getEntity() instanceof Player)){
 			Player player = (Player) e.getDamager();
 			Creature mob = (Creature) e.getEntity();
 			
@@ -47,7 +130,30 @@ public class CombatMechanics implements Listener{
 				player.sendMessage(ChatColor.RED + "" + dmg + " -> " + mob.getCustomName() + 
 						ChatColor.WHITE + " [" + ChatColor.GREEN + "0" + ChatColor.WHITE + "]");
 			}
+		} else if (e.getDamager() instanceof Player && e.getEntity() instanceof Player){
+			Player player = (Player) e.getDamager();
+			Player ent = (Player) e.getEntity();
+			
+			int dmg = (int) e.getDamage();
+			int hp = (int) ent.getHealth() - dmg;
+			
+			if (hp > 0){
+				ent.setHealth(hp);
+			} else {
+				ent.setHealth(0);
+			}
+
+			e.setDamage(0);
+			
+			if (hp >= 1){
+			player.sendMessage(ChatColor.RED + "" + dmg + " -> " + ent.getName() + 
+					ChatColor.WHITE + " [" + ChatColor.GREEN + hp + ChatColor.WHITE + "]");
+			} else {
+				
+				player.sendMessage(ChatColor.RED + "" + dmg + " -> " + ent.getName() + 
+						ChatColor.WHITE + " [" + ChatColor.GREEN + "0" + ChatColor.WHITE + "]");
+			}
 		}
+		
 	}
-	
 }
