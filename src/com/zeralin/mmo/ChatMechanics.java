4302f8f500dbbf4b935a730a3e4bed789d16f2b0
Zeralin <zeralinservers@yahoo.com>
2016-04-06 04:02:41 +0100

    gg

---
+++ src/com/zeralin/mmo/ChatMechanics.java
@@ -0,0 +1,13 @@
+package com.zeralin.mmo;
+
+import org.bukkit.event.Listener;
+
+public class ChatMechanics implements Listener{
+
+	public Main main;
+	
+	public ChatMechanics(Main plugin){
+		main = plugin;
+	}
+	
+}
